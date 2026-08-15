543266316e8cdf754a1805f4c46c10a6f427f917
Maxel347 <max.ammerbuch@icloud.com>
2026-08-15 14:19:03 +0200

    patched john pork

--- src/main/java/com/example/examplemod/entity/client/PorkusRenderer.java
+++ src/main/java/com/example/examplemod/entity/client/PorkusRenderer.java
@@ -1,26 +1,33 @@
 package com.example.examplemod.entity.client;
 
 import com.example.examplemod.entity.costom.PorkusEntity;
-import com.example.examplemod.entity.costom.RhinoEntity;
 import com.example.examplemod.maxmodcool;
 import com.mojang.blaze3d.vertex.PoseStack;
-import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.model.PiglinModel;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.resources.ResourceLocation;
 
-public class PorkusRenderer extends MobRenderer<RhinoEntity, RhinoModel<RhinoEntity>> {
+public class PorkusRenderer extends MobRenderer<PorkusEntity, PorkusModel<PorkusEntity>> {
+
     public PorkusRenderer(EntityRendererProvider.Context pContext) {
-        super(pContext, new RhinoModel<>(pContext.bakeLayer(ModModelLayers.PORKUS_LAYER)), 2f);
+        super(pContext, new PorkusModel<>(pContext.bakeLayer(ModModelLayers.PORKUS_LAYER)), 0.5f);
     }
-
-
-
 
     @Override
-    public ResourceLocation getTextureLocation(RhinoEntity rhinoEntity) {
-        return ResourceLocation.fromNamespaceAndPath(maxmodcool.MOD_ID, "textures/entity/rhino.png");
+    public ResourceLocation getTextureLocation(PorkusEntity entity) {
+        return ResourceLocation.fromNamespaceAndPath(maxmodcool.MOD_ID, "textures/entity/porkus.png");
+    }
 
+    @Override
+    protected void setupRotations(PorkusEntity pEntityLiving, PoseStack pMatrixStack, float pAgeInTicks, float pRotationYaw, float pPartialTicks) {
+        super.setupRotations(pEntityLiving, pMatrixStack, pAgeInTicks, pRotationYaw, pPartialTicks);
+
+        PorkusModel<PorkusEntity> model = this.getModel();
+
+        float earWobble = net.minecraft.util.Mth.sin(pAgeInTicks * 0.5f) * 5;
+
+        model.right_ear.zRot = earWobble;
+        model.left_ear.zRot = -earWobble;
     }
 }
-
